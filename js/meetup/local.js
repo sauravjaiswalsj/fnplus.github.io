27225cf02f00940b84b600c0788fdcd56f6f1de9
R3l3ntl3ss <dev.91096@gmail.com>
2018-12-19 09:57:12 +0530

    Fixed Meetup http error

--- js/meetup/local.js
+++ js/meetup/local.js
@@ -1,9 +1,11 @@
 jQuery.noConflict();
 var mup_widget = {
-    with_jquery: function(block) {
+    with_jquery: function (block) {
         block(jQuery, document.body);
     },
-    api_call: function(path, params) {
-        return "http://api.meetup.com" + path + "?callback=?&" + jQuery.param(jQuery.extend({ key: $api_key }, params));
+    api_call: function (path, params) {
+        return "https://api.meetup.com" + path + "?callback=?&" + jQuery.param(jQuery.extend({
+            key: $api_key
+        }, params));
     }
 };
